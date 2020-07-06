6d9d627cdb4784b42f6c407b5ffc0205f8fc47e5
Harpreet Singh <hr557871@dal.ca>
2020-07-05 21:00:49 -0300

    adding models for Jobparts, Search and Users table

--- companyZ/company-z/api/models/Search.js
+++ companyZ/company-z/api/models/Search.js
@@ -1,5 +1,5 @@
 /**
- * Jobparts.js
+ * Search.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
@@ -11,25 +11,17 @@ module.exports = {
 
     updatedAt: false,
     createdAt: false,
-    tableName: 'jobparts',
+    tableName: 'search',
     jobName: {
       type: 'string', required: true
-    },
-    partId: {
-      type: 'number', required: true
-    },
-    qty: {
-      type: 'number', required: true,
     },
     date: {
       type: 'string', required: true,
     },
     time: {
       type: 'string', required: true,
-    },
-    result: {
-      type: 'string', required: true
     }
   },
-  datastores: 'jobparts',
+  datastores: 'search',
 };
+
---
+++ companyZ/company-z/api/models/Users.js
@@ -0,0 +1,26 @@
+/**
+ * Users.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+
+  attributes: {
+    updatedAt: false,
+    createdAt: false,
+    tableName: 'users',
+    id: {
+      type: 'string',
+      columnName: 'user',
+      required: true
+    },
+    password: {
+      type: 'string',
+      required: true
+    }
+  },
+  datastores: 'users',
+};
+
