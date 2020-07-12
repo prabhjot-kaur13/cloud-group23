59df93da13195455c046a41552be7dd5ad3e7d00
Rajveen Chandhok <rj660238@dal.ca>
2020-07-12 00:05:52 -0300

    Fixed bug where order was not being placed inspite of quantity available with company Y

--- companyZ/company-z/api/controllers/JobsController.js
+++ companyZ/company-z/api/controllers/JobsController.js
@@ -110,11 +110,11 @@ module.exports = {
         // return res.status(400).send("Due to high demand, we are only allowing ordering of one job per user.");
         return res.view('pages/order', {
           result: 'failure',
-          message: 'Due to high demand, we are only allowing ordering of one job per user'
+          message: 'Due to high demand, we are allowing ordering of only one job per user'
         });
     }
     
-    let orderSuccess = postData.tableData.every(element => element.qoh >= element.qty);
+    let orderSuccess = postData.tableData.every(element => parseInt(element.qoh) >= parseInt(element.qty));
 
     let datetime = new Date().toISOString();
     postData.tableData.forEach(element => {
