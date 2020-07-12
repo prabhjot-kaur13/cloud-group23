b202b59d328479b630717b950f63a6ba82a5da8a
Rajveen Chandhok <rj660238@dal.ca>
2020-07-11 21:45:37 -0300

    Updated controllers with actual endpoints

--- companyZ/company-z/api/controllers/JobsController.js
+++ companyZ/company-z/api/controllers/JobsController.js
@@ -10,44 +10,70 @@ const axios = require('axios');
 module.exports = {
 
   list: function(req, res) {
-    // axios.get('http://companyx-env.eba-azstb5mp.us-east-1.elasticbeanstalk.com/API735/getJobs')
-    // .then(response => {
-    //     console.log(response);
-    //     // res.send(response.data);
-    //     res.view('pages/list', {jobs: response.data})
-    // })
-    // .catch(error => {
-    //     console.log(error);
-    // });
-    let data = [{'id':'job383','partId':383,'qty':33},
-      {'id':'job383','partId':384,'qty':35},
-      {'id':'job541','partId':541,'qty':55},
-      {'id':'job691','partId':691,'qty':66},
-      {'id':'job705','partId':705,'qty':78},
-      {'id':'job735','partId':735,'qty':77},
-      {'id':'job930','partId':930,'qty':99}];
-    res.view('pages/list', {jobs: data});
+    axios.get('http://companyx-env.eba-c2pbkmaf.us-east-1.elasticbeanstalk.com/API735/getJobs')
+    .then(response => {
+        // console.log(response);
+        res.view('pages/list', {jobs: response.data})
+    })
+    .catch(error => {
+        console.log(error);
+        res.view('pages/order', {
+          result: 'failure',
+          message: 'Error in fetching jobs'
+        });
+    });
+    // let data = [{'id':'job383','partId':383,'qty':33},
+    //   {'id':'job383','partId':384,'qty':35},
+    //   {'id':'job541','partId':541,'qty':55},
+    //   {'id':'job691','partId':691,'qty':66},
+    //   {'id':'job705','partId':705,'qty':78},
+    //   {'id':'job735','partId':735,'qty':77},
+    //   {'id':'job930','partId':930,'qty':99}];
+    // res.view('pages/list', {jobs: data});
   },
 
   parts: function(req, res) {
     let jobName = req.params.jobName;
+
+    axios.get('http://companyx-env.eba-c2pbkmaf.us-east-1.elasticbeanstalk.com/API735/getJobByJobName/' + jobName)
+    .then(response => {
+        let partIds = [];
+        response.data.forEach(part => {
+          partIds.push(parseInt(part.partId))
+        });
+
+        let parts = {partId: partIds};
+        let jobDetails = response.data;
+
+        axios.post('http://129.173.67.218:1337/viewParts', parts)
+          .then(response => {
+            let partDetails = response.data;
+            res.view('pages/parts', {parts: partDetails, jobDetails: jobDetails, jobName: jobName});
+          })
+          .catch(error => {
+            console.log(error);
+            res.view('pages/order', {
+              result: 'failure',
+              message: 'Error in fetching parts'
+            });
+          });
+        })
+    .catch(error => {
+        console.log(error);
+        res.view('pages/order', {
+          result: 'failure',
+          message: 'Job not found'
+        });
+    });
+ 
     // let parts = {"partId": [383, 384]};
-    // axios.post('http://129.173.67.218:1337/viewParts', parts)
-    // .then(response => {
-    //     console.log(response);
-    //     // res.send(response.data);
-    //     res.view('pages/parts', {parts: response.data, jobName: jobName});
-    // })
-    // .catch(error => {
-    //     console.log(error);
-    // });
 
-    let jobDetails = [{"id":"job383","partId":383,"qty":33},
-      {"id":"job383","partId":384,"qty":35}];
-    let partDetails = [{"partid":383,"partName":"mouse","qoh":38},
-      {"partid":384,"partName":"printer","qoh":30}];
+    // let jobDetails = [{"id":"job383","partId":383,"qty":33},
+    //   {"id":"job383","partId":384,"qty":35}];
+    // let partDetails = [{"partId":383,"partName":"mouse","qoh":38},
+    //   {"partId":384,"partName":"printer","qoh":40}];
 
-    res.view('pages/parts', {parts: partDetails, jobDetails: jobDetails, jobName: jobName});
+    // res.view('pages/parts', {parts: partDetails, jobDetails: jobDetails, jobName: jobName});
   },
 
   validate: async function (req, res) {
@@ -114,30 +140,30 @@ module.exports = {
       console.log({partOrders});
 
       //Inform company X about the successful order
-      // axios.post('http://companyx-env.eba-c2pbkmaf.us-east-1.elasticbeanstalk.com/API735/createPartsOrder', partOrders)
-      //   .then(response => {
-      //     console.log("=================Company X success begin===================");
-      //     console.log(response);
-      //     console.log("=================Company X success end===================");
-      //   })
-      //   .catch(error => {
-      //     console.log("=================Company X failed begin===================");
-      //     console.log(error);
-      //     console.log("=================Company X failed end===================");
-      //   });
+      axios.post('http://companyx-env.eba-c2pbkmaf.us-east-1.elasticbeanstalk.com/API735/createPartsOrder', partOrders)
+        .then(response => {
+          console.log("=================Company X success begin===================");
+          console.log(response);
+          console.log("=================Company X success end===================");
+        })
+        .catch(error => {
+          console.log("=================Company X failed begin===================");
+          console.log(error);
+          console.log("=================Company X failed end===================");
+        });
 
         //Inform company Y about the successful order
-        // axios.post('http://129.173.67.223:3000/parts541/updatePartOrders', {orders: partOrders})
-        // .then(response => {
-        //   console.log("=================Company Y success begin===================");
-        //   console.log(response);
-        //   console.log("=================Company Y success end===================");
-        // })
-        // .catch(error => {
-        //   console.log("=================Company Y failed begin===================");
-        //   console.log(error);
-        //   console.log("=================Company Y failed end===================");
-        // });
+        axios.post('http://129.173.67.218:1337/parts541/updatePartOrders', {orders: partOrders})
+        .then(response => {
+          console.log("=================Company Y success begin===================");
+          console.log(response);
+          console.log("=================Company Y success end===================");
+        })
+        .catch(error => {
+          console.log("=================Company Y failed begin===================");
+          console.log(error);
+          console.log("=================Company Y failed end===================");
+        });
 
       res.view('pages/order', {
         result: 'success',
--- companyZ/company-z/api/controllers/SearchController.js
+++ companyZ/company-z/api/controllers/SearchController.js
@@ -17,12 +17,17 @@ module.exports = {
         if (err) {
           res.status(500).send("Database Error");
         }
-        res.status(200).send('Job Found: Entry added');
+        //res.status(200).send('Job Found: Entry added');
+        res.redirect('/jobs/job/' + job);
       });
     })
     .catch(error => {
       if(error.response.status === 404) {
-        res.status(404).send('Job Not found');
+        // res.status(404).send('Job Not found');
+        res.view('pages/order', {
+          result: 'failure',
+          message: 'Job not found'
+        });
       }
     });
     // await Search.create({jobName: job, date: datetime.slice(0,10), time: datetime.slice(11, 19)}).intercept((err)=>{
